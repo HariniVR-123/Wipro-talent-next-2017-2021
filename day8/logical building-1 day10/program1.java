36a7d245960d76f362f95f91da19d01257471b30
HariniVR-123 <65912317+HariniVR-123@users.noreply.github.com>
2020-06-08 19:31:34 +0530

    Create program1.java

---
+++ day8/logical building-1 day10/program1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode
+{
+	public String secondWordUpperCase
+	{
+		String[] a=input1.split(" ");
+        	if(a.length<=1)
+        	{
+           		return "LESS";
+        	}
+        	String word=a[1].toUpperCase();
+        	return word;
+    	}
+}
